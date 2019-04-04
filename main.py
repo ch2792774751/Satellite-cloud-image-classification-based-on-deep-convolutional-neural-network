4fdf2b98c1d0af6cd249a7ace30350f450db6f2f
ch2792774751 <31959887+ch2792774751@users.noreply.github.com>
2019-04-04 18:17:15 +0800

    Update main.py

--- main.py
+++ main.py
@@ -14,11 +14,15 @@ from keras.callbacks import History
 from Config import Config
 from resnet import *
 from lenet import *
-from utils.py import *
+from utils import *
+
+lr_reducer = ReduceLROnPlateau(factor=np.sqrt(0.1), cooldown=0, patience=5, min_lr=0.1e-6)
+early_stopper = EarlyStopping(min_delta=0.01, patience=100)
+
 gpu_options = tf.GPUOptions(allow_growth=True)
 set_session(tf.Session(config=tf.ConfigProto(gpu_options=gpu_options)))
 
-fr1 = open('./train_test.pickle','rb')   
+fr1 = open('./train2.pickle','rb')   
 trainData, trainLabels,testData, testLabels =  pickle.load(fr1)
 testLabels = testLabels.reshape([-1,])
 trainLabels = trainLabels.reshape([-1,])
@@ -33,8 +37,8 @@ print('测试集个数',testData.shape[0])
 trainLabels = np_utils.to_categorical(trainLabels, Config.CLASSES)  
 testLabels = np_utils.to_categorical(testLabels, Config.CLASSES)
 
-resnet = resnet_model(Config.CLASSES,Config.INPUT_SHAPE)
-sgd = SGD(lr=Config.LEARNING_RATE, decay=config.WEIGHT_DECAY, momentum=config.LEARNING_MOMENTUM, nesterov=True)
+resnet = resnet(Config.CLASSES,Config.INPUT_SHAPE)
+sgd = SGD(lr=Config.LEARNING_RATE, decay=Config.WEIGHT_DECAY, momentum=Config.LEARNING_MOMENTUM, nesterov=True)
 #resnet.compile(loss='categorical_crossentropy',optimizer=Adam(),metrics=['accuracy'])
 resnet.compile(optimizer=sgd, loss='categorical_crossentropy', metrics=['accuracy'])  
 resnet.summary()
@@ -42,17 +46,21 @@ resnet.summary()
 time_start = time.time()
 print("开始训练resnet神经网络模型")
 history = History()
-history=resnet.fit(trainData,trainLabels,batch_size=config.BATCH_SIZE,epochs=config.EPOCH,verbose=1,shuffle=True,validation_data=(testData,testLabels),callbacks=[lr_reducer, early_stopper])
+history=resnet.fit(trainData,trainLabels,batch_size=Config.BATCH_SIZE,epochs=Config.EPOCH,verbose=1,shuffle=True,validation_data=(testData,testLabels),callbacks=[lr_reducer, early_stopper])
 resnet.save('./resnet_epochs.h5')
 scores = resnet.evaluate(testData,testLabels,verbose=0)
 print("scores = ",scores)
 time_end = time.time()
 print("resnet神经网络模型训练结束")
 print('resnet神经网络模型训练的时间是:',time_end - time_start,'s')
-plot_model(resnet,to_file="./model/resnet50.png",show_shapes=True)
+#plot_model(resnet,to_file="./model/resnet50.png",show_shapes=True)
 save_data(file_dir='./result/' ,filename='val_loss',data=history.history["val_loss"])
 save_data(file_dir='./result/' ,filename='train_loss',data=history.history["loss"])
+save_data(file_dir='./result/' ,filename='val_acc',data=history.history["val_acc"])
+save_data(file_dir='./result/' ,filename='train_acc',data=history.history["acc"])
+
 plot_fig(history.history["loss"], history.history["val_loss"],'train_loss', 'val_loss')
+plot_fig(history.history["acc"], history.history["val_acc"],'train_acc', 'val_acc')
 # fig, ax1 = plt.subplots(1,1)
 # ax1.plot(history.history["val_loss"])
 # ax1.plot(history.history["loss"])
