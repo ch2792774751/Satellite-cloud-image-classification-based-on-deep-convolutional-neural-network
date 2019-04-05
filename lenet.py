66c8a9a2c2676b6483404bf3d7b0da39b180a5b9
ch2792774751 <31959887+ch2792774751@users.noreply.github.com>
2019-04-05 15:35:21 +0800

    Update lenet.py

--- lenet.py
+++ lenet.py
@@ -7,26 +7,21 @@ import keras.layers as KL
 import keras.models as KM
 import keras.utils as keras_utils
 
-
 def Lenet(out_class, input_shape):
     inputs = KL.Input(shape = input_shape)
-    x = KL.Conv2D(filters= 10, kernel_size= (2,2), strides= (1, 1), padding= 'same',activation= 'relu',use_bias=True)(inputs)
-    x = KL.MaxPooling2D(pool_size= (2, 2), strides= (2, 2), padding= 'same')(x)
-    x = KL.Conv2D(filters= 20, kernel_size= (2,2), strides= (1, 1), padding= 'same', activation= 'relu', use_bias=True)(x)
-    x = KL.MaxPooling2D(pool_size= (2, 2), strides= (2, 2), padding= 'valid')(x)
-    x = KL.Conv2D(filters= 40, kernel_size= (2,2), strides= (1, 1), padding= 'same', activation= 'relu', use_bias=True)(x)
+    x = KL.Conv2D(filters= 16, kernel_size= [2, 2], strides= (1, 1), padding= 'same',activation= 'relu')(inputs)
+    x = KL.MaxPooling2D(pool_size= (2, 2))(x)
+    #x.Dropout(0.5)(x)
+    x = KL.Conv2D(filters= 62, kernel_size= (2, 2), padding= 'same', activation= 'relu')(x)
+    x = KL.MaxPooling2D(pool_size= (2, 2))(x)
+    #x.Dropout(0.5)(x)
+    x = KL.Conv2D(filters= 64, kernel_size= (2, 2), strides= (1, 1), padding= 'same', activation= 'relu')(x)
     x = KL.Flatten()(x)
-    x = KL.Dense(10, activation= keras.activations.relu, use_bias=True)(x)
-    out = KL.Dense(out_class, activation= 'softmax', use_bias=True)(x)
+    x = KL.Dense(10, activation='relu')(x)
+    out = KL.Dense(out_class, activation= 'softmax',)(x)
     model = KM.Model(inputs= inputs, outputs = out)
     return model
 
 if __name__ == "__main__":
     model = Lenet(4,(28,28,4))
     model.summary()
-
-
-
-
-
-
